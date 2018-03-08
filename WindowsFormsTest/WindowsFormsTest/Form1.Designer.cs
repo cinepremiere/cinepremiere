bc35f3cf492eeb7cf30644530bfce122d0a48eb6
noelaramartinez <36252157+noelaramartinez@users.noreply.github.com>
2018-03-07 21:08:28 -0600

    commit
    nuevo

--- WindowsFormsTest/WindowsFormsTest/Form1.Designer.cs
+++ WindowsFormsTest/WindowsFormsTest/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.TxtContrasena = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -44,29 +45,26 @@
             // LblCuenta
             // 
             this.LblCuenta.AutoSize = true;
-            this.LblCuenta.Location = new System.Drawing.Point(145, 50);
-            this.LblCuenta.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblCuenta.Location = new System.Drawing.Point(109, 41);
             this.LblCuenta.Name = "LblCuenta";
-            this.LblCuenta.Size = new System.Drawing.Size(57, 17);
+            this.LblCuenta.Size = new System.Drawing.Size(43, 13);
             this.LblCuenta.TabIndex = 0;
             this.LblCuenta.Text = "Usuario";
             // 
             // TxtUsuario
             // 
-            this.TxtUsuario.Location = new System.Drawing.Point(20, 82);
-            this.TxtUsuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtUsuario.Location = new System.Drawing.Point(15, 67);
             this.TxtUsuario.Name = "TxtUsuario";
-            this.TxtUsuario.Size = new System.Drawing.Size(311, 22);
+            this.TxtUsuario.Size = new System.Drawing.Size(234, 20);
             this.TxtUsuario.TabIndex = 1;
             this.TxtUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.TxtUsuario.TextChanged += new System.EventHandler(this.TxtUsuario_TextChanged);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(352, 314);
+            this.pictureBox1.Size = new System.Drawing.Size(264, 255);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
@@ -79,21 +77,18 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.TxtUsuario);
             this.groupBox1.Controls.Add(this.LblCuenta);
-            this.groupBox1.Location = new System.Drawing.Point(16, 336);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 273);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(352, 271);
+            this.groupBox1.Size = new System.Drawing.Size(264, 220);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(84, 196);
-            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Location = new System.Drawing.Point(63, 159);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(169, 17);
+            this.linkLabel1.Size = new System.Drawing.Size(127, 13);
             this.linkLabel1.TabIndex = 6;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Reestablecer Contraseña";
@@ -101,10 +96,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(199, 235);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(149, 191);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 28);
+            this.button2.Size = new System.Drawing.Size(100, 23);
             this.button2.TabIndex = 4;
             this.button2.Text = "&Ingresar";
             this.button2.UseVisualStyleBackColor = true;
@@ -112,10 +106,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(20, 235);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(15, 191);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 28);
+            this.button1.Size = new System.Drawing.Size(112, 23);
             this.button1.TabIndex = 5;
             this.button1.Text = "&Salir";
             this.button1.UseVisualStyleBackColor = true;
@@ -123,34 +116,41 @@
             // 
             // TxtContrasena
             // 
-            this.TxtContrasena.Location = new System.Drawing.Point(20, 154);
-            this.TxtContrasena.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtContrasena.Location = new System.Drawing.Point(15, 125);
             this.TxtContrasena.Name = "TxtContrasena";
             this.TxtContrasena.PasswordChar = '*';
-            this.TxtContrasena.Size = new System.Drawing.Size(311, 22);
+            this.TxtContrasena.Size = new System.Drawing.Size(234, 20);
             this.TxtContrasena.TabIndex = 3;
             this.TxtContrasena.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(145, 122);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(109, 99);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Contraseña";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
             // FrmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 640);
+            this.ClientSize = new System.Drawing.Size(288, 520);
             this.ControlBox = false;
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmLogin";
@@ -161,6 +161,7 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -175,6 +176,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox TxtContrasena;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
