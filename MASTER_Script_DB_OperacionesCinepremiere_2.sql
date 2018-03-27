430b7ad99a8c76ecaebb49da614f64babafdb3c5
noelaramartinez <36252157+noelaramartinez@users.noreply.github.com>
2018-03-26 22:50:28 -0600

    update master_cine_premier noe lara

--- MASTER_Script_DB_OperacionesCinepremiere_2.sql
+++ MASTER_Script_DB_OperacionesCinepremiere_2.sql
@@ -4,6 +4,12 @@ use DB_OperacionesCinepremium_2
 go
 set dateformat 'dmy'
 go
+create table TipoUsuario
+(
+	claveTipoUsuario smallint primary key,
+	tipoUsuario nvarchar(25) not null,--1 = Adminstrador, 2 = Gerente, 3 = Supervisor de Cajas, 4 = Cajero, 5 = TEST/ROOT
+)
+go
 create table Usuarios
 (
 	claveUsuario nvarchar(5) primary key,
@@ -11,7 +17,7 @@ create table Usuarios
 	segundoNombre nvarchar(20) null,
 	paterno nvarchar(25) not null,
 	materno nvarchar(25) not null,--
-	tipoUsuario smallint not null,--1 = Adminstrador, 2 = Gerente, 3 = Supervisor de Cajas, 4 = Cajero, 5 = TEST/ROOT
+	claveTipoUsuario smallint references TipoUsuario(claveTipoUsuario) not null,
 	calle nvarchar(25) not null,
 	numeroExterior nvarchar(5) not null,
 	numeroInterior nvarchar (5) null,
@@ -24,29 +30,40 @@ create table Usuarios
 	turno smallint not null,
 	mail nvarchar(50) not null,
 	suledo money not null,
-	constrasena nvarchar (10) not null
+	constrasena nvarchar (10) not null,
+	status bit not null,
 )
 go
 create table Idioma
 (
-	idIdioma smallint primary key,
-	idioma char(5) not null,
-	subtitulos char(5) not null
+	claveIdioma smallint primary key,
+	idioma char(15) not null,
+	status bit not null,
 )
-
-create table Tipo
+go
+create table Subtitulos
 (
-	idTipo smallint primary key,
-	tipo char(5) not null
+	claveSubtitulos smallint primary key,
+	subtitulos char(15) not null,
+	status bit not null,
 )
-
+go
+create table TipoFuncion
+(
+	claveTipoFuncion smallint primary key,
+	tipoFuncion char(15) not null,--regular, premier, especial,vip...
+	precio money not null,
+	descripcion char(15) not null,--2d,3d,4dx,matine
+	status bit not null,
+)
+go
 create table Checador
 (
-	claveUsuario nvarchar(5) references Usuarios(claveUsuario),
-	horaIngreso date,
-	horaSalidaComida date,
-	horaregresoComida date,
-	horasalida date 
+	claveUsuario nvarchar(5) references Usuarios(claveUsuario) not null,
+	horaIngreso date not null,
+	horaSalidaComida date not null,
+	horaregresoComida date not null,
+	horasalida date not null,
 )
 go
 create table Clientes
@@ -67,14 +84,30 @@ create table Clientes
 	fechaRegistro date not null,
 	mail nvarchar(50) not null,
 	constrasena nvarchar (10) not null,
-	puntos int null
+	puntos int null,
+	status bit not null,
+	tipoCliente int references TipoCliente(claveTipoCliente) not null,
+)
+go
+create table TipoCliente
+(
+	claveTipoCliente int primary key,
+	tipoCliente char(15) not null,
+	status bit not null,
 )
 go
 create table Salas
 (
-	claveSala nvarchar(5) primary key,
+	claveSala char(5) primary key,
+	claveTipoSala smallint references TipoSala(claveTipoSala),
+	status bit not null,
+)
+go
+create table TipoSala
+(
+	claveTipoSala smallint primary key,
 	capacidad smallint not null,
-	tipo nvarchar (30) not null
+	descripcion nvarchar(50),
 )
 go
 create table Peliculas
@@ -86,7 +119,7 @@ create table Peliculas
 	fechaEstreno date not null,
 	fechaRetiro date not null,
 	duracion time not null,
-	disponible bit not null
+	disponible bit not null,
 )
 go
 create table Funciones
@@ -94,12 +127,11 @@ create table Funciones
 	claveFuncion nvarchar(5) primary key,
 	claveSala nvarchar (5) references Salas(claveSala) not null,
 	clavePelicula nvarchar (5) references Peliculas (clavePelicula) not null,
-	idTipo smallint references Tipo(idTipo),
-	idIdioma smallint references Idioma(idIdioma),
+	claveTipoFuncion smallint references TipoFuncion(claveTipoFuncion),
+	claveIdioma smallint references Idioma(idIdioma),
+	claveSubtitulos smallint references Subtitulos(claveSubtitulos),
 	fecha date not null,
 	hora time not null,
-	idioma nvarchar(20) not null,
-	tipo nvarchar(20) not null,
 	disponible bit not null,
 	lugaresLibres smallint not null,
 )
@@ -109,8 +141,9 @@ create table Boletos
 	boleto nvarchar(5) primary key,
 	claveFuncion nvarchar (5) references Funciones (claveFuncion) not null,
 	asiento nvarchar(3) not null,
+	fila char(3) not null,
 	tipoCliente bit not null,
-	precioUnitario money not null,
+	fechaHoraImpresion date not null,
 )
 go
 create table Ventas
@@ -119,13 +152,17 @@ create table Ventas
 	claveCliente nvarchar(5) references Clientes(claveCliente) null,
 	claveUsuario nvarchar(5) references Usuarios(claveUsuario) not null,
 	fechaVenta date not null,
-	horaVenta time not null
+	horaVenta time not null,
+	importeTotal money not null,
  )
  go
  create table DetallesVentas
  (
 	claveVenta nvarchar(5) references Ventas(claveVenta),
-	claveFuncion nvarchar(5) references Funciones(claveFuncion),
-	numeroBoleto nvarchar(5) references Boletos(boleto) not null
+	boleto nvarchar(5) references Boletos(boleto) not null,
+	descuento money,
+	iva money,
+	precioUnitario money not null,
+	importeParcial money not null,
  )
  go
